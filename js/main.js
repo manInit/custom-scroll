02ccdccff3190893f7c642537626c062e1f8a230
man_init <37896566+manInit@users.noreply.github.com>
2020-08-14 21:58:13 +0800

    fully rewrite code

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-scrollCreate('scroll1', 'scroll__block', 'scroll__line', 'content');
-scrollCreate('scroll2', 'scroll__block_2', 'scroll__line_2', 'content_2');
+const scroll = new Scroll('scroll1');
+const scroll2 = new Scroll('scroll2', 1);
--- js/scroll.js
+++ js/scroll.js
@@ -1,93 +1,96 @@
-function scrollCreate(rootId, blockClass, lineClass, contentClass) {
+class Scroll {
+	constructor(blockId, mode = 0) {
+		this._scrollRoot = document.getElementById(blockId);
+		this._scrollLine = this._createElement(mode);
+		this._scrollBlock = this._scrollLine.firstElementChild;
 
-	const scrollRoot = document.getElementById(rootId);
-	const scrollBlock = document.getElementsByClassName(blockClass)[0];
-	const scrollBar = document.getElementsByClassName(lineClass)[0];
-	const scrollContent = document.getElementsByClassName(contentClass)[0];
+		this._content = this._scrollRoot.firstElementChild;
 
+		this._scrollRoot.appendChild(this._scrollLine);
 
+		this._scrollStep = 50;
 
-	mainCycle(scrollRoot, scrollBlock, scrollBar, scrollContent);
-	checkResize(scrollRoot, scrollBlock, scrollBar, scrollContent);
-}
+		this._scrollLineSize = mode === 0 ?
+		  this._scrollLine.clientHeight - this._scrollBlock.offsetHeight - 2 * parseInt(getComputedStyle(this._scrollLine).paddingBottom) :
+		  this.srollLineSize =  this._scrollLine.clientWidth - this._scrollBlock.offsetWidth - 2 * parseInt(getComputedStyle(this._scrollLine).paddingRight); 		
 
-function mainCycle(scrollRoot, scrollBlock, scrollBar, scrollContent) {
-	//начальное задание позиций
-	scrollBlock.style.right = 0 + 'px';
-	scrollContent.style.left = 0 + 'px';
-	//количество скроллов для полного прокручивания
-	const scrollCount = 9;
-	const barWidth = scrollBar.offsetWidth;
-	//меняем ширину блока
-	scrollBlock.style.width = barWidth / scrollCount + 'px';
-	const blockWidth = scrollBlock.offsetWidth;
-	//максимальное значение позиции блока
-	const maxPosition = blockWidth - barWidth;
-	//шаг прокручивания блока
-	const stepBlock = -maxPosition / scrollCount;
-	//ширина скрытого контента (отрицательное)
-	const hiddenContentWidth = scrollRoot.clientWidth - scrollContent.offsetWidth;
+		this._sizeOverflow = mode === 0 ? 
+		  this._content.offsetHeight - this._scrollRoot.clientHeight :
+		  this._content.offsetWidth - this._scrollRoot.clientWidth
 
-	//сдвигание контента при изменении положение скролла
-	function scroll(blockPosition) {
-		let contentPosition;
-		//если выходит за границы
-		if (blockPosition <= maxPosition) {
-			blockPosition = maxPosition;
-			contentPosition = hiddenContentWidth;
-		}
-		else if (blockPosition >= 0) {
-			blockPosition = 0;
-			contentPosition = 0;
-		}
-		//если все хорошо сдвигаем конент на шаг
-		else {
-			contentPosition = blockPosition * hiddenContentWidth / maxPosition;
-		}
+		this._blockStep = this._scrollLineSize * this._scrollStep / this._sizeOverflow;
+		this.scrollState = 0;
+		this.blockState = 0;
 
-		scrollContent.style.left = contentPosition + 'px';
-		scrollBlock.style.right = blockPosition + 'px';
-	}
-	//перемещние мышкой
-	scrollBlock.addEventListener('mousedown', e => {
-		e.preventDefault();
-		let shiftX = e.clientX - scrollBlock.clientWidth;
-		//событие при перемещении
-		function onMouseMove(e) {
-			scroll(-e.pageX + shiftX);
-		}
-		//удаляем обработчики при отпускании мыши
-		function onMouseUp(e) {
-			document.removeEventListener('mousemove', onMouseMove);
-			document.removeEventListener('mouseup', onMouseUp);
-		}
-		//вешаем обработчики
-		document.addEventListener('mousemove', onMouseMove);
-		document.addEventListener('mouseup', onMouseUp);
-		//отменяем дефолтное событие на драге
-		scrollBlock.ondragstart = () => false;
-	});
-
-	//прокручивание колесиком
-	scrollBar.addEventListener('wheel', e => {
-		e.preventDefault();
-		let left = parseInt(scrollBlock.style.right);
-
-		if (e.deltaY > 0) left -= stepBlock;
-		else left += stepBlock;
+		this._initStyle(mode);
 		
-		scroll(left);
-	});
-}
+		if (mode === 1) this._scrollRoot.addEventListener('wheel', this._onScrollWidth.bind(this))
+		else this._scrollRoot.addEventListener('wheel', this._onScrollHeight.bind(this))
+	}
 
-function checkResize(scrollRoot, scrollBlock, scrollBar, scrollContent) {
-	let firstsize = scrollContent.offsetWidth;
-	setInterval(() => {
-		if (firstsize != scrollContent.offsetWidth) {
-			firstsize = scrollContent.offsetWidth;
-			mainCycle(scrollRoot, scrollBlock, scrollBar, scrollContent);
+	_initStyle(mode) {
+		this._content.style.position = 'relative';
+		if (mode === 1) {
+			this._content.style.right = 0;
+			this._scrollBlock.style.right = 0;
+		} else {
+			this._content.style.top = 0;
+			this._scrollBlock.style.top = 0;
 		}
-	}, 300)
+	}
+
+	_createElement(mode) {
+		const elem = document.createElement('div');
+		elem.classList.add('scroll__line', mode === 1 ? 'scroll__line-width' : 'scroll__line-height');
+
+		const block = document.createElement('div');
+		block.classList.add('scroll__block');
+
+		elem.appendChild(block);
+
+		return elem;
+	}
+
+	_onScrollWidth(e) {
+		e.preventDefault();
+		if (e.deltaY > 0) {
+			this.scrollState -= this._scrollStep;
+			if (this.scrollState < 0) this.scrollState = 0;
+
+			this.blockState += this._blockStep;
+			if (this.blockState > 0) this.blockState = 0;
+		}
+		else {
+			this.scrollState += this._scrollStep;
+			if (this.scrollState > this._sizeOverflow) this.scrollState = this._sizeOverflow;
+
+			this.blockState -= this._blockStep;
+			if (this.blockState < -this._scrollLineSize) this.blockState = -this._scrollLineSize;
+		}
+
+		this._content.style.right = `${this.scrollState}px`;
+		this._scrollBlock.style.right = `${this.blockState}px`;
+	}
+
+	_onScrollHeight(e) {
+		e.preventDefault();
+
+		if (e.deltaY > 0) {
+			this.scrollState -= this._scrollStep;
+			if (this.scrollState < -this._sizeOverflow) this.scrollState = -this._sizeOverflow;
+
+			this.blockState += this._blockStep;
+			if (this.blockState > this._scrollLineSize) this.blockState = this._scrollLineSize;
+		}
+		else {
+			this.scrollState += this._scrollStep;
+			if (this.scrollState > 0) this.scrollState = 0;
+
+			this.blockState -= this._blockStep;
+			if (this.blockState < 0) this.blockState = 0;
+		}
+
+		this._content.style.top = `${this.scrollState}px`;
+		this._scrollBlock.style.top = `${this.blockState}px`;
+	}
 }
-
-
